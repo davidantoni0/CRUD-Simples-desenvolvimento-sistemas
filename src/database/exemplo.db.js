46059fbb130e84bdffc9d6e7edc80c807ffe06f3
David Antônio Gonçalves <pet0104207@aluno.firjansenaisesi.com.br>
2026-03-26 21:19:16 -0300

    Adicionando conexão com o banco de dados local

---
+++ src/database/exemplo.db.js
@@ -0,0 +1,12 @@
+/*
+import {Pool} from 'pg'
+
+export const db = new Pool({
+  user: NOME_DO_SEU_USUARIO,
+  password: SUA_SENHA,
+  host: "localhost",
+  port: 5432,
+  database: SEU_BANCO
+});
+
+*/
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import rotasUsuario from "./routes/usuarios.routes.js"
 import rotasLivro from "./routes/livros.routes.js"
+import {db} from "./database/db.js"
 
 const PORT = 3000
 const api = express()
@@ -8,6 +9,13 @@ const api = express()
 api.use( express.json() )
 api.use( rotasUsuario )
 api.use( rotasLivro )
+
+api.get("/usuarios2", async function(req,res){
+	const resultado = await db.query("SELECT * from usuarios;")
+
+	res.status(200).json(resultado.rows)
+})
+
 api.listen(PORT, () => {
 	console.log(`Servidor rodando em http://localhost:${PORT}`)
 })
